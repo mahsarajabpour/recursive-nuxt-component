f52839209d52d994fd87143b4f500bc4d549ded3
mahsa <mahsaa.rajabpour@gmail.com>
2024-05-25 23:46:20 +0330

    add(sidebar component)

---
+++ components/Sidebar.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex items-center border h-[75px] px-4">
+    <p class="text-4xl font-bold">Nuxi</p>
+  </div>
+</template>
+
+<style scoped></style>
--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -1,13 +1,27 @@
 <script setup lang="ts">
+  import Sidebar from "~/components/Sidebar.vue";
   useHead({
     meta: [{ name: "Dashboard" }],
   });
 </script>
 
 <template>
-  <div id="dashboard-layout">
-    <slot class="content" />
+  <div
+    id="dashboard-layout"
+    class="dashboard"
+  >
+    <sidebar />
+    <div class="p-4 h-full">
+      <slot class="content" />
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+  .dashboard {
+    background-image: url("assets/images/background-image.jpg");
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100vh;
+  }
+</style>
