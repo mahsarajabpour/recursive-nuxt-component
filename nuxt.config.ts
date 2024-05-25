17f510347aca30a4209f2cb851d33f964dc80ec8
mahsa <mahsaa.rajabpour@gmail.com>
2024-05-25 23:41:19 +0330

    add(styles)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: ["assets/styles/tailwind.css", "assets/styles/main.css"],
+
   devtools: { enabled: true },
   modules: ["@pinia/nuxt"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 });
