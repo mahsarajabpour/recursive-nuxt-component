4a7300fd81324bc22efd92f02936d1b6c7ed97fc
mahsa <mahsaa.rajabpour@gmail.com>
2024-05-26 08:48:11 +0330

    improvements: add header and close btn for sidebar

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -4,8 +4,33 @@
   const sidebarStore = useSidebarStore();
 </script>
 <template>
-  <div v-if="sidebarStore.status === 'active'">
-    {{ sidebarStore.items }}
+  <div
+    v-if="sidebarStore.status === 'active'"
+    class="relative top-[-76px] w-1/3 bg-white border"
+  >
+    <div class="flex items-center p-2 bg-gray-200">
+      <div class="mr-auto text-2xl">Sidebar items</div>
+      <button @click="sidebarStore.status = 'inactive'">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="size-12 ml-auto"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+          />
+        </svg>
+      </button>
+    </div>
+
+    <div>
+      {{ sidebarStore.items }}
+    </div>
   </div>
 </template>
 
